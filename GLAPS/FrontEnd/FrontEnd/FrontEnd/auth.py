8077372cd17c47cf2ca943ccd7b4b31eb75e497d
Brian Schweikart <schweikb0866@student.faytechcc.edu>
2019-02-20 00:09:57 -0500

    more db

--- GLAPS/FrontEnd/FrontEnd/FrontEnd/auth.py
+++ GLAPS/FrontEnd/FrontEnd/FrontEnd/auth.py
@@ -38,41 +38,31 @@ def load_logged_in_user():
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
-	"""Register a new user.
-	Validates that the username is not already taken. Hashes the
-	password for security.
-	"""
-	if request.method == 'POST':
-		username = request.form['username']
-		email = request.form['yourEmail']
-        #password = request.form['password'] #this is throwing error
-		db = get_db()
-		error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        db = get_db()
+        error = None
 
-		if not username:
-			error = 'Username is required.'
-		elif not email:
-			error = 'Email is required.'
-   #     elif not password: # error thrown
-			#error = 'Password is required.'
-		elif db.execute(
-			'SELECT id FROM user WHERE username = ?', (username,)
-		).fetchone() is not None:
-			error = 'User {0} is already registered.'.format(username)
+        if not username:
+            error = 'Username is required.'
+        elif not email:
+            error = 'Email is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM users WHERE username = ?', (username,)
+            ).fetchone() is not None:
+            error = 'User {0} is already registered.'.format(username)
 
-		if error is None:
-			# the name is available, store it in the database and go to
-			# the login page
-			db.execute(
-				'INSERT INTO user (username, yourEmail, password) VALUES (?, ?)',
-				(username, generate_password_hash(password))
-			)
-			db.commit()
-			return redirect(url_for('login'))
+        if error is None:
+            db.execute('INSERT INTO users (username, email, password) VALUES (?, ?',(username, generate_password_hash(password)))
+            db.commit() 
+            return redirect(url_for('login'))
 
-		flash(error)
-
-	return render_template('register.html')
+                    #flash(error)
+    return render_template('register.html')
 
 
 @bp.route('/login', methods=('GET', 'POST'))
--- GLAPS/FrontEnd/FrontEnd/FrontEnd/db.py
+++ GLAPS/FrontEnd/FrontEnd/FrontEnd/db.py
@@ -3,12 +3,12 @@ import click
 from flask import current_app, g
 from flask.cli import with_appcontext
 
-sqlite_file = 'users.sqlite.sql' #Needs to be changed to a data base not just a schema place holder
-conn = sqlite3.connect(sqlite_file)
-c = conn.cursor()
+#sqlite_file = 'glaps.sqlite' #Needs to be changed to a data base not just a schema place holder
+#conn = sqlite3.connect(sqlite_file)
+#c = conn.cursor()
 
-conn.commit()
-conn.close()
+#conn.commit()
+#conn.close()
 
 def init_app(app):
 	"""Register database functions with the Flask app. This is called by
@@ -18,7 +18,7 @@ def init_app(app):
 	app.cli.add_command(init_db_command)
 
 def get_db():
-    sqlite_file = 'users.sqlite.sql' #Needs to be changed to a data base not just a schema place holder
+    sqlite_file = 'glaps.sqlite' #Needs to be changed to a data base not just a schema place holder
     conn = sqlite3.connect(sqlite_file)
     c = conn.cursor()
 
