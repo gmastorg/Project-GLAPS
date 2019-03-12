8dab4f2c77d2c3ddc2b75aa856b248392963175d
Brian Schweikart <schweikb0866@student.faytechcc.edu>
2019-03-12 02:17:08 -0400

    code clean up

--- GLAPS/FrontEnd/FrontEnd/FrontEnd/static/scripts/script.js
+++ GLAPS/FrontEnd/FrontEnd/FrontEnd/static/scripts/script.js
@@ -1,11 +1,9 @@
 var $ = jQuery;
 
-jQuery(document).ready(function($) {  
-
-    $(window).load(function(){
-        $('#preloader').fadeOut('slow',function(){$(this).remove();});
+jQuery(document).ready(function ($) {
+    $(window).load(function () {
+        $('#preloader').fadeOut('slow', function () { $(this).remove(); });
     });
-
 });
 
 // Setting up Video
@@ -14,17 +12,16 @@ var $ = jQuery;
 var vidWidth = $(window).width(), vidHeight = $(window).height();
 
 $("#video-wrap").css({
-    
     'height': vidHeight
 });
 
 $('#video').videoBG({
-    mp4:'assets/bg.mp4',
-    ogv:'assets/bg.ogv',
-    webm:'assets/bg.webm',
-    poster:'assets/poster.jpg',
-    scale:true,
-    zIndex:0,
+    mp4: 'assets/bg.mp4',
+    ogv: 'assets/bg.ogv',
+    webm: 'assets/bg.webm',
+    poster: 'assets/poster.jpg',
+    scale: true,
+    zIndex: 0,
     height: vidHeight
 });
 
@@ -32,44 +29,41 @@ $('#video').videoBG({
 
 $("#nav-menu").stick_in_parent()
 
-
 // Calling Wow
 
 new WOW().init();
 
-
 // Count Down Timer
 
 $('.countdown').final_countdown({
-    start : 1360000000, //Here use Milisecond. To convert your time you can go to this(https://currentmillis.com/) website. 
-    end   : 1390000000,
-    now : 1385000000,
-seconds: {
-borderColor: '#5677fc',
-borderWidth: '3'
-},
-minutes: {
-    borderColor: '#7e57c2',
-    borderWidth: '3'
-},
-hours: {
-    borderColor: '#4db6ac',
-    borderWidth: '3'
-},
-days: {
-    borderColor: '#d81b60',
-    borderWidth: '3'
-}}, function() {
+    start: 1360000000, //Here use Milisecond. To convert your time you can go to this(https://currentmillis.com/) website.
+    end: 1390000000,
+    now: 1385000000,
+    seconds: {
+        borderColor: '#5677fc',
+        borderWidth: '3'
+    },
+    minutes: {
+        borderColor: '#7e57c2',
+        borderWidth: '3'
+    },
+    hours: {
+        borderColor: '#4db6ac',
+        borderWidth: '3'
+    },
+    days: {
+        borderColor: '#d81b60',
+        borderWidth: '3'
+    }
+}, function () {
 });
-
 
 // rotating text
 
-(function() {
-
+(function () {
     var quotes = $(".quotes");
     var quoteIndex = -1;
-    
+
     function showNextQuote() {
         ++quoteIndex;
         quotes.eq(quoteIndex % quotes.length)
@@ -77,40 +71,36 @@ days: {
             .delay(1000)
             .fadeOut(1000, showNextQuote);
     }
-    
+
     showNextQuote();
-    
 })();
 
 // smooth mouse wheel
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
+$(function () {
+    $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
 });
-
 
 /*------------------------------------------
  Subscribe form ajax
  ------------------------------------------*/
 
-
-$('#subscribe-form').submit(function(e) {
-
+$('#subscribe-form').submit(function (e) {
     e.preventDefault();
-    var $form           = $('#subscribe-form');
-    var submit          = $('#subscribe-button');
-    var ajaxResponse    = $('#subscription-response');
-    var email           = $('#subscriber-email').val();
+    var $form = $('#subscribe-form');
+    var submit = $('#subscribe-button');
+    var ajaxResponse = $('#subscription-response');
+    var email = $('#subscriber-email').val();
 
     $.ajax({
         type: 'POST',
@@ -120,11 +110,11 @@ $('#subscribe-form').submit(function(e) {
             email: email
         },
         cache: false,
-        beforeSend: function(result) {
+        beforeSend: function (result) {
             submit.val("Joining...");
         },
-        success: function(result) {
-            if(result.sendstatus == 1) {
+        success: function (result) {
+            if (result.sendstatus == 1) {
                 ajaxResponse.html(result.message);
                 $form.fadeOut(500);
             } else {
@@ -133,5 +123,4 @@ $('#subscribe-form').submit(function(e) {
             }
         }
     });
-
 });
