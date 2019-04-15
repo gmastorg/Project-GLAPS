2ac560fb9112fa81edb4860abe9234630d6e9232
gmastorg <31484932+gmastorg@users.noreply.github.com>
2019-04-15 13:28:01 -0400

    changes to glaps
    input validation list of States and Counties

--- FrontEnd/FrontEnd/FrontEnd/views.py
+++ FrontEnd/FrontEnd/FrontEnd/views.py
@@ -139,8 +139,10 @@ def contact():
         year=datetime.now().year,
         message='Please Contact us with any questions or concerns.')
 
+app.config["DEBUG"] = True
 @app.route('/glaps', methods=["GET","POST"]) #this section is used for when the data bases are linked.
 def glaps():
+    State_Counties= getState_CountiesList()
     errors=""
     if request.method == "POST":
         County = None
@@ -148,34 +150,36 @@ def glaps():
         try:
             County = request.form['County']
         except:
-            errors += "<p>{!r} is empty.</p>\n".format(request.form["County"])
+            errors += "Please Enter County".format(request.form["County"])
         try:
             HomeVal = int(request.form["HomeVal"])
         except:
-            errors += "<p>{!r} is not a number.</p>\n".format(request.form["HomeVal"])
+            errors += "Please Enter Home Value".format(request.form["HomeVal"])
         if County is not None and HomeVal is not None:
-            result = getAPI()
-            result = list(result[0].values())
+            if County is not None:
+                result = getAPI()
+                result = list(result[0].values())
 
-            actualNoStad = str("{:,}".format(result[0]))
-            actualWStad = str("{:,}".format(result[1]))
-            medianNoStad = str("{:,}".format(result[2]))
-            medianWStad = str("{:,}".format(result[3]))
+                actualNoStad = str("{:,}".format(result[0]))
+                actualWStad = str("{:,}".format(result[1]))
+                medianNoStad = str("{:,}".format(result[2]))
+                medianWStad = str("{:,}".format(result[3]))
 
-            output = Markup("Current Home Value without a Stadium:   " + '<font color="limegreen">$' +actualNoStad+ '</font>' +  \
-            "<br><br>Current Home Value with a Stadium:   " +  '<font color="limegreen">$' +actualWStad+ '</font>' + \
-           "<br><br>Median Value of Homes in " + '<font color="yellow">'+ County + '</font>' +" without a Stadium:   " +  '<font color="limegreen">$' +medianNoStad+ '</font>' + \
-           "<br><br>Median Value of Homes in " + '<font color="yellow">'+ County + '</font>'+ " with a Stadium:   "  +'<font color="limegreen">$' +medianWStad+ '</font>' + \
-           "<br><br><br><small>The predicted values have a PERCENT margin of error and were calculated using data from the 2017 U.S. Census</small>")
-            return render_template('glaps.html',
-            title='Home Value Predictor',
-            bytearray=datetime.now().year,
-            message=output)
-    else: 
-        return render_template('glaps.html',
+                output = Markup("Current Home Value without a Stadium:   " + '<font color="limegreen">$' +actualNoStad+ '</font>' +  \
+                "<br><br>Current Home Value with a Stadium:   " +  '<font color="limegreen">$' +actualWStad+ '</font>' + \
+               "<br><br>Median Value of Homes in " + '<font color="yellow">'+ County + '</font>' +" without a Stadium:   " +  '<font color="limegreen">$' +medianNoStad+ '</font>' + \
+               "<br><br>Median Value of Homes in " + '<font color="yellow">'+ County + '</font>'+ " with a Stadium:   "  +'<font color="limegreen">$' +medianWStad+ '</font>' + \
+               "<br><br><br><small>The predicted values have a PERCENT margin of error and were calculated using data from the 2017 U.S. Census</small>")
+                return render_template('glaps.html',
+                title='Home Value Predictor',
+                bytearray=datetime.now().year,
+                message=output)
+    
+    return render_template('glaps.html',
         title='Home Value Predictor',
         bytearray=datetime.now().year,
-        message= 'Enter your location on the map and your current home value below:')
+        message= 'Enter your location on the map and your current home value below:',
+        errors=errors)
 
 @app.route('/test')
 def test():
@@ -198,3 +202,21 @@ def getAPI():
     responseJson = json.loads(url.text)
 
     return responseJson
+
+import os
+import csv
+
+def getState_CountiesList():
+    
+    States_Counties = []
+    path = os.path.abspath("States_Counties.csv")
+    with open(path) as file:
+        inputFile = csv.reader(file)
+        
+        for row in inputFile:
+             State_County=row[0]
+             States_Counties.append(movie)
+     
+    States_Counties.pop(0)
+    
+    return States_Counties
